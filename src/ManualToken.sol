f5fb83c0b1a4550900f0edd165a365a8cd303482
dariodesalvo <dariodesalvo@gmail.com>
2024-08-07 01:33:17 -0300

    fix mapping

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -3,22 +3,21 @@
 pragma solidity 0.8.19;
 
 contract ManualToken {
+    mapping(address => uint256) private s_balances;
 
-    mapping (address => uint) private s_balances;
-
-    function name() public pure returns (string memory){
+    function name() public pure returns (string memory) {
         return "Dari's token";
     }
 
-    function totalSupply() public pure returns (uint256){
+    function totalSupply() public pure returns (uint256) {
         return 100 ether;
     }
 
-    function decimals() public pure returns (uint8){
+    function decimals() public pure returns (uint8) {
         return 18;
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance){
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return s_balances[_owner];
     }
 
@@ -28,5 +27,4 @@ contract ManualToken {
         s_balances[_to] += _amount;
         require(balanceOf(msg.sender) + balanceOf(_to) == previousBalance);
     }
-
 }
